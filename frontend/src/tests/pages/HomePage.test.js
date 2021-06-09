bcff788923fae8467809475d7fe17ae5c51b97ef
Phillip Conrad <pconrad@cs.ucsb.edu>
2021-06-09 13:25:54 -0700

    pc/sv - remove commented out test

--- frontend/src/tests/pages/HomePage.test.js
+++ frontend/src/tests/pages/HomePage.test.js
@@ -9,25 +9,7 @@ import commonsFixtures from "fixtures/commonsFixtures";
 
 describe("HomePage tests", () => {
     const queryClient = new QueryClient();
-    // test("renders without crashing when get returns null", () => {
-    //     var axiosMock = new AxiosMockAdapter(axios);
-    //     axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
-        
-    //     axiosMock.onGet("/api/commons").reply(200, null);
-    //     const { getByTestId } = render(
-    //         <QueryClientProvider client={queryClient}>
-    //             <MemoryRouter>
-    //                 <HomePage />
-    //             </MemoryRouter>
-    //         </QueryClientProvider>
-    //     );
-
-    //     const title = getByTestId("homePage-title");
-    //     expect(title).toBeInTheDocument();
-    //     expect(typeof(title.textContent)).toBe('string');
-    //     expect(title.textContent).toEqual('Howdy Farmer');
-    // });
-
+    
     test("renders without crashing when lists return empty list", () => {
         var axiosMock = new AxiosMockAdapter(axios);
         axiosMock.onGet("/api/currentUser").reply(200, apiCurrentUserFixtures.userOnly);
