db959c7334f17003e4225d3a260bb2ad126d0a76
Phillip Conrad <pconrad@cs.ucsb.edu>
2021-06-09 12:22:18 -0700

    pc/aa/team - various updates to the play page components, and storybook for those

--- frontend/src/fixtures/commonsFixtures.js
+++ frontend/src/fixtures/commonsFixtures.js
@@ -8,9 +8,6 @@ const commonsFixtures = {
             "endDate":"6/11/2021",
             "totalPlayers": 50,
             "cowPrice": 15,
-            //cowHealth and totalWealth need to be in user fixture eventually 
-            "cowHealth": 98,
-            "totalWealth": 300
         }
     ]
 }
